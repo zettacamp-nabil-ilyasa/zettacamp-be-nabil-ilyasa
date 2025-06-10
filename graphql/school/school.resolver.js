f7abd19269c3eb52c3ecfbf312acd461dcb368df
zettacamp-nabil-ilyasa <nabil.ilyasa@zettacamp.pro>
2025-06-10 11:42:40 +0700

    [Module_1][CRUD School][ZEBN-5] school CRUD rework

--- graphql/school/school.resolver.js
+++ graphql/school/school.resolver.js
@@ -4,49 +4,47 @@ const School = require('./school.model.js')
 const {NameIsExist} = require('../helper/helper.js')
 const {CleanUpdateInput} = require('../../utils/validator.js')
 
-const schoolResolvers = {
-    School: {
-         students: (parent, _, context) => {
-            const schoolId = parent._id?.toString()|| parent.id?.toString()
-            console.log("Resolving students for school:", schoolId)
-            return context.loaders.studentLoader.load(schoolId)
-        }
-},
-    Query: {
-        GetAllSchools: async () => {
-            return await School.find({status : 'active'})
-        },
-        GetOneSchool: async (_, {id}) => {
-            return await School.findOne({_id : id, status : 'active'})
-        }
-    },
-    Mutation: {
-        CreateSchool: async (_, {input}) => {
-            const {brand_name, long_name, address} = input
-            const nameIsExist = await NameIsExist(School, long_name)
-            if (nameIsExist) {
-                throw new Error('School name already exist')
-            }
-            const newSchool = new School({brand_name, long_name, address, status: 'active'})
-            await newSchool.save()
-            const createdSchool = await School.findOne({long_name})
-            return createdSchool
-        },
-        UpdateSchool: async (_, {input}) => {
-            const {id, brand_name, long_name, address} = input
-            const nameIsExist = await NameIsExist(School, long_name, id)
-            if (nameIsExist) {
-                throw new Error('School name already exist')
-            }
-            const updatedSchool = CleanUpdateInput(input)
-            return await School.findOneAndUpdate({_id : id}, updatedSchool, {new: true})
-        },
-        DeleteSchool: async (_, {id}) => {
-            await School.findOneAndUpdate({_id : id}, {deleted_at : new Date(), status: 'deleted'})
-            return 'School deleted successfully'
-        }
+//****************QUERY**************** 
+async function GetAllSchools() {
+    return await School.find({status : 'active'})
+}
+async function GetOneSchool(_, {id}) {
+    return await School.findOne({_id : id, status : 'active'})
+}
+
+//****************MUTATION**************** 
+async function CreateSchool(_, {input}) {
+    const {brand_name, long_name, address} = input
+    const nameIsExist = await NameIsExist(School, long_name)
+    if (nameIsExist) {
+        throw new Error('School name already exist')
     }
+    const newSchool = new School({brand_name, long_name, address, status: 'active'})
+    await newSchool.save()
+    const createdSchool = await School.findOne({long_name})
+    return createdSchool
+}
+async function UpdateSchool(_, {input}) {
+    const {id} = input
+    const updatedSchool = CleanUpdateInput(input)
+    return await School.findOneAndUpdate({_id : id}, updatedSchool, {new: true})
+}
+async function DeleteSchool(_, {id}) {
+    await School.findOneAndUpdate({_id : id}, {deleted_at : new Date(), status: 'deleted'})
+    return 'School deleted successfully'
+}
+
+//***************FIELD RESOLVER***************
+async function StudentsFieldResolver(parent, _, context) {
+    const schoolId = parent._id?.toString()
+    return context.loaders.studentLoader.load(schoolId)
 }
 
 // *************** EXPORT MODULE *************** 
-module.exports = schoolResolvers
+module.exports = {
+    Query: {GetAllSchools, GetOneSchool},
+    Mutation: {CreateSchool, UpdateSchool, DeleteSchool},
+    School: {
+        students: StudentsFieldResolver
+    }
+}
