5ef10548242b7c46d09f257a958eba3e265853b8
zettacamp-nabil-ilyasa <nabil.ilyasa@zettacamp.pro>
2025-07-10 11:47:39 +0700

    [MODULE_2][ZEBN-14][BE_011] Implement CRUD Resolvers for Subject Model : Create Subject Typedef

---
+++ modules/subject/subject.typedef.js
@@ -0,0 +1,54 @@
+// *************** IMPORT LIBRARY ***************
+const { gql } = require('apollo-server-express');
+
+const subjectTypeDefs = gql`
+  type Subject {
+    _id: ID!
+    block_id: Block!
+    name: String!
+    description: String
+    coefficient: Int!
+    test_ids: [Test!]
+    status: Status!
+    created_at: Date!
+    created_by: User!
+    updated_at: Date!
+    deleted_at: Date
+    deleted_by: User
+  }
+
+  input SubjectInput {
+    name: String!
+    description: String
+    coefficient: Int!
+    block_id: String
+  }
+
+  input SubjectFilterInput {
+    block_id: String
+  }
+
+  input PaginationInput {
+    limit: Int
+    offset: Int
+  }
+
+  enum Status {
+    active
+    deleted
+  }
+
+  extend type Query {
+    GetAllSubjects(filter: SubjectFilterInput, pagination: PaginationInput): [Subject]
+    GetOneSubject(_id: ID!): Subject
+  }
+
+  extend type Mutation {
+    CreateSubject(input: SubjectInput): Subject
+    UpdateSubject(_id: ID!, input: SubjectInput): Subject
+    DeleteSubject(_id: ID!): String
+  }
+`;
+
+// *************** EXPORT MODULE ***************
+module.exports = subjectTypeDefs;
