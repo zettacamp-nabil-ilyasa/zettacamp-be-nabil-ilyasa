709a4f0749083636b523b0ad42fe7e952a31a510
zettacamp-nabil-ilyasa <nabil.ilyasa@zettacamp.pro>
2025-07-10 15:24:45 +0700

    [MODULE_2][ZEBN-15][BE_012] Implement CRUD Resolvers for Subject Model :Create Subject Validator
    + control flag for validator

--- modules/subject/subject.validators.js
+++ modules/subject/subject.validators.js
@@ -1,16 +1,26 @@
 // *************** IMPORT LIBRARY ***************
 const { ApolloError } = require('apollo-server-express');
 
+// *************** IMPORT VALIDATOR ***********************
+const { ValidateMongoObjectId } = require('../../utilities/validators/mongo-validator.js');
+
 /**
- *
+ * Validate subject input
  * @param {Object} inputObject - The input containing subject data
  * @param {String} inputObject.name - The name of subject data
  * @param {String} inputObject.description - The description of subject data
  * @param {Number} inputObject.coefficient - The coefficient for calculation factor (not yet implemented)
+ * @param {Object} [option] - Optional control flag to exclude block_id from checking (for update mutation)
+ * @param {Object} [option.checkBlockId] - Parameter to control validation flow
  */
-function ValidateSubjectInput(inputObject) {
+function ValidateSubjectInput(inputObject, { checkBlockId } = {}) {
   // *************** destructured input object
-  const { name, description, coefficient } = inputObject;
+  const { name, description, coefficient, block_id } = inputObject;
+
+  // *************** validate block_id if checkBlockId set to true
+  if (checkBlockId) {
+    ValidateMongoObjectId(block_id);
+  }
 
   // *************** validate subject's name
   if (!name || typeof name !== 'string') throw new ApolloError('name is required and must be a string');
@@ -22,5 +32,7 @@ function ValidateSubjectInput(inputObject) {
   // *************** validate subject's coefficient
   if (description && typeof description !== 'string') throw new ApolloError('description must be a string');
 }
+
+async function ValidateBlockExistence(blockId) {}
 
 module.exports = { ValidateSubjectInput };
