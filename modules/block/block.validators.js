c856bba39615539e53bf8ef3360ac4c6503f375c
zettacamp-nabil-ilyasa <nabil.ilyasa@zettacamp.pro>
2025-07-10 10:20:39 +0700

    [MODULE_2][ZEBN-14][BE_003] Implement CRUD Resolvers for Block Model : Create Block Validator

---
+++ modules/block/block.validators.js
@@ -0,0 +1,20 @@
+// *************** IMPORT LIBRARY ***************
+const { ApolloError } = require('apollo-server-express');
+const BlockModel = require('./block.model.js');
+
+/**
+ *
+ * @param {Object} input - The input containing block data
+ * @param {Object} input.name - The name of block data
+ * @param {Object} input.description - The description of block data
+ */
+function ValidateBlockInput(input) {
+  // *************** destructured input object
+  const { name, description } = input;
+
+  // *************** validate name
+  if (!name || typeof name !== 'string') throw new ApolloError('name is required and must be a string');
+  if (description && typeof description !== 'string') throw new ApolloError('description must be a string');
+}
+
+module.exports = { ValidateBlockInput };
