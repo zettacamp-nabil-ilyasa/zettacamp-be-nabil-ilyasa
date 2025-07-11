8402e1521114d43dbd622875ce638c38531c37bd
zettacamp-nabil-ilyasa <nabil.ilyasa@zettacamp.pro>
2025-07-11 13:55:16 +0700

    [MODULE_2][ZEBN-12][BE_048] Define Task Management Schema: Create GetAllTasks

---
+++ modules/task/task.resolver.js
@@ -0,0 +1,65 @@
+// *************** IMPORT LIBRARY ***************
+const { ApolloError } = require('apollo-server-express');
+
+// *************** IMPORT MODULE ***************
+const TaskModel = require('./test.model.js');
+const ErrorLogModel = require('../errorLog/error_log.model.js');
+
+// *************** IMPORT VALIDATOR ***********************
+const { ValidateTaskFilterInput } = require('./task.validators.js');
+const { ValidateMongoObjectId } = require('../../utilities/validators/mongo-validator.js');
+const { ValidatePaginationInput } = require('../../utilities/validators/pagination-validator.js');
+
+// **************** QUERY ****************
+/**
+ * Get all tasks with optional filtering by type, status and pagination.
+ * @async
+ * @function GetAllTasks
+ * @param {Object} params - The parameter object
+ * @param {Object} [filterInput] - Optional filter input
+ * @param {String} [filterInput.type] - Optional type to filter tasks
+ * @param {String} [filterInput.status] - Optional status to filter tasks
+ * @param {Object} [paginationInput] - Optional pagination input
+ * @param {Number} [paginationInput.limit] - Number of tasks per page
+ * @param {Number} [paginationInput.offset] - Number of tasks to skip
+ * @returns {Promise<Array<Object>>} Array of test documents matching the query
+ * @throws {ApolloError} If any error occurs during validation or database operation
+ */
+async function GetAllTasks({ filterInput, paginationInput }) {
+  try {
+    // **************** validate status and type within filterInput
+    ValidateTaskFilterInput(filterInput);
+
+    // **************** validate limit and offset within paginationInput
+    ValidatePaginationInput(paginationInput);
+
+    // **************** build base query
+    const query = { status: { $ne: 'deleted' } };
+
+    // **************** build query for subject_id if it exist
+    if (filterInput?.type) {
+      query.type = filterInput.type;
+    }
+
+    // **************** build query for status if it exist
+    if (filterInput?.status) {
+      query.status = filterInput.status;
+    }
+
+    // **************** execute query
+    const tasks = await TaskModel.find(query)
+      .skip(paginationInput?.offset || 0)
+      .limit(paginationInput?.limit || 20)
+      .sort({ created_at: -1 })
+      .lean();
+    return tasks;
+  } catch (error) {
+    await ErrorLogModel.create({
+      error_stack: error.stack,
+      function_name: 'GetAllTasks',
+      path: '/modules/task/task.resolver.js',
+      parameter_input: JSON.stringify({ filterInput, paginationInput }),
+    });
+    throw new ApolloError(error.message);
+  }
+}
