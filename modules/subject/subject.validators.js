cc78d659547745ef34f386d9d2fdd10bab90bca9
zettacamp-nabil-ilyasa <nabil.ilyasa@zettacamp.pro>
2025-07-14 08:48:42 +0700

    [MODULE_2][ZEBN-15][BE_012] Implement CRUD Resolvers for Subject Model :Create Subject Validator

--- modules/subject/subject.validators.js
+++ modules/subject/subject.validators.js
@@ -1,7 +1,7 @@
 // *************** IMPORT LIBRARY ***************
 const { ApolloError } = require('apollo-server-express');
 
-// *************** IMPORT VALIDATOR ***********************
+// *************** IMPORT VALIDATOR ***************
 const { ValidateMongoObjectId } = require('../../utilities/validators/mongo-validator.js');
 
 /**
@@ -13,12 +13,12 @@ const { ValidateMongoObjectId } = require('../../utilities/validators/mongo-vali
  * @param {Object} [option] - Optional control flag to exclude block_id from checking (for update mutation)
  * @param {Object} [option.checkBlockId] - Parameter to control validation flow
  */
-function ValidateSubjectInput(inputObject, { checkBlockId } = {}) {
+function ValidateSubjectInput(inputObject, { validateBlockId } = {}) {
   // *************** destructured input object
   const { name, description, coefficient, block_id } = inputObject;
 
   // *************** validate block_id if checkBlockId set to true
-  if (checkBlockId) {
+  if (validateBlockId) {
     ValidateMongoObjectId(block_id);
   }
 
@@ -26,13 +26,11 @@ function ValidateSubjectInput(inputObject, { checkBlockId } = {}) {
   if (!name || typeof name !== 'string') throw new ApolloError('name is required and must be a string');
 
   // *************** validate subject's coefficient
-  if (!coefficient || typeof coefficient !== 'number') throw new ApolloError('name is required and must be a number');
+  if (typeof coefficient !== 'number') throw new ApolloError('coefficient is required and must be a number');
   if (coefficient < 0 || coefficient === 0) throw new ApolloError('coefficient must be a positive number and cannot be 0');
 
   // *************** validate subject's coefficient
   if (description && typeof description !== 'string') throw new ApolloError('description must be a string');
 }
-
-async function ValidateBlockExistence(blockId) {}
-
+// *************** EXPORT MODULE ***************
 module.exports = { ValidateSubjectInput };
