97573f6ba17d412fd17e70d9eeaf9fbf170a9126
zettacamp-nabil-ilyasa <nabil.ilyasa@zettacamp.pro>
2025-07-14 08:22:14 +0700

    [MODULE_2][ZEBN-18][BE_046] Define StudentTestResult Model: Create GetTestNotation

--- modules/studenTestResult/student_test_result.helper.js
+++ modules/studenTestResult/student_test_result.helper.js
@@ -5,6 +5,40 @@ const { ApolloError } = require('apollo-server-express');
 const TestModel = require('../test/test.model.js');
 const TaskModel = require('../task/task.model.js');
 const ErrorLogModel = require('../errorLog/error_log.model.js');
+
+// *************** IMPORT VALIDATOR ***************
+const { ValidateMongoObjectId } = require('../../utilities/validators/mongo-validator.js');
+
+/**
+ * Retrieve notations from a published test.
+ * @async
+ * @param {string} testId - The id of the test document.
+ * @returns {Promise<Array>} - Array of notation objects.
+ * @throws {ApolloError} - If test not found or error occurs during DB operation.
+ */
+async function GetTestNotations(testId) {
+  try {
+    // *************** validate test's id
+    ValidateMongoObjectId(testId);
+
+    // *************** get test document
+    const testDocument = await TestModel.findOne({ _id: testId, status: 'published' });
+    if (!testDocument) {
+      throw new ApolloError('test not found');
+    }
+
+    return testDocument.notations;
+  } catch (error) {
+    await ErrorLogModel.create({
+      error_stack: error.stack,
+      function_name: 'GetTestNotations',
+      path: '/modules/studentTestResult/studentTestResult.helper.js',
+      parameter_input: JSON.stringify({ testId }),
+    });
+    throw new ApolloError(error.message);
+  }
+}
+
 /**
  * Set status of an enter_marks task to 'completed'.
  * @async
