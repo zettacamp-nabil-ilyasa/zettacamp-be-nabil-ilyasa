3d878bbc8f69383d3b0dd118ce12f907cf753b73
zettacamp-nabil-ilyasa <nabil.ilyasa@zettacamp.pro>
2025-07-14 08:20:12 +0700

    [MODULE_2][ZEBN-18][BE_045] Define StudentTestResult Model: Create SetEnterMarksTaskToValidated

---
+++ modules/studenTestResult/student_test_result.helper.js
@@ -0,0 +1,35 @@
+// *************** IMPORT LIBRARY ***************
+const { ApolloError } = require('apollo-server-express');
+
+// *************** IMPORT MODULE ***************
+const TestModel = require('../test/test.model.js');
+const TaskModel = require('../task/task.model.js');
+const ErrorLogModel = require('../errorLog/error_log.model.js');
+/**
+ * Set status of an enter_marks task to 'completed'.
+ * @async
+ * @param {string} taskId - The Id of the task to update.
+ * @returns {Promise<void>}
+ * @throws {ApolloError} - If update fails or task is not modified.
+ */
+async function SetEnterMarksTaskToCompleted(taskId) {
+  try {
+    // *************** validate task id
+    ValidateMongoObjectId(taskId);
+
+    // *************** update task document
+    const updatedTask = await TaskModel.updateOne({ _id: taskId, status: 'in_progress' }, { $set: { status: 'completed' } });
+    if (updatedTask.modifiedCount === 0) {
+      throw new ApolloError('failed to update task');
+    }
+  } catch (error) {
+    await ErrorLogModel.create({
+      error_stack: error.stack,
+      function_name: 'SetEnterMarksTaskToCompleted',
+      path: '/modules/studentTestResult/studentTestResult.helper.js',
+      parameter_input: JSON.stringify({ taskId }),
+    });
+    throw new ApolloError(error.message);
+  }
+}
+
