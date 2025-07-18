1beae634d83cede627725910240dc44924a7ef3c
zettacamp-nabil-ilyasa <nabil.ilyasa@zettacamp.pro>
2025-07-18 14:37:44 +0700

    [Module_3][BE-010][ZEBN-27] Create ValidateSubjectPassConditionInput

--- modules/subject/subject.validators.js
+++ modules/subject/subject.validators.js
@@ -39,5 +39,70 @@ function ValidateSubjectFilterInput(filterInput) {
   if (filterInput?.block_id) ValidateMongoObjectId(filterInput.block_id);
 }
 
+/**
+ * Validate pass conditions input
+ * @param {Array<Object>} subjectPassConditionsInput - an array of object containing pass/fail criteria
+ * @param {String} parameter - pass condition's parameter to be used for conditional checking
+ * @param {Number} parameter_value - pass condition's parameter_value to be used as comparator
+ * @param {String} syllabus_type - pass condition's syllabus_type
+ * @param {String} test_id - id of Test used within pass_conditions
+ *@param  {String} math_operator - string representation of math_operator
+ * @param {String} logical_operator - string representation of logical operator
+ * @throws {ApolloError} - if validation fails
+ */
+function ValidateSubjectPassConditionsInput(subjectPassConditionsInput) {
+  if (!Array.isArray(subjectPassConditionsInput))
+    subjectPassConditionsInput.forEach((passCondition, index) => {
+      // *************** validate pass condition's parameter
+      if (!passCondition.parameter || typeof passCondition.parameter !== 'string') {
+        throw new ApolloError(`pass condition's parameter in ${index} must be a string`);
+      }
+
+      // *************** validate pass condition's parameter_value
+      if (!passCondition.parameter_value || typeof passCondition.parameter_value !== 'number') {
+        throw new ApolloError(`pass condition's parameter_value in ${index} must be a number`);
+      }
+
+      // *************** validate pass condition's syllabus_type
+      if (!passCondition.syllabus_type || typeof passCondition.syllabus_type !== 'string') {
+        throw new ApolloError(`pass condition's syllabus_type in ${index} must be a string`);
+      }
+
+      // *************** validate pass condition's math_operator
+      if (!passCondition.math_operator || typeof passCondition.math_operator !== 'string') {
+        throw new ApolloError(`pass condition's math_operator in ${index} must be a string`);
+      }
+      if (!mathOperatorEnum.includes(passCondition.math_operator)) {
+        throw new ApolloError(`pass condition's math_operator must be one of following: ${mathOperatorEnum.join(', ')}`);
+      }
+
+      // *************** apply some rules for parameter 'mark'
+      if (passCondition.parameter === 'mark') {
+        // *************** validate test_id if syllabus_type is 'test'
+        if (passCondition.syllabus_type === 'test') {
+          ValidateMongoObjectId(passCondition.test_id);
+        }
+
+        // *************** throw error if syllabus_type is subject
+        if (passCondition.syllabus_type === 'subject') {
+          throw new ApolloError(`pass condition's 'mark' cannot be used for syllabus_type 'subject' in ${index}`);
+        }
+      }
+
+      // *************** validate pass condition's parameter 'average_of_marks'
+      if (passCondition.parameter === 'average_of_marks') {
+        // *************** throw error if syllabus_type is test
+        if (passCondition.syllabus_type === 'test') {
+          throw new ApolloError(`pass condition's 'mark' cannot be used for syllabus_type 'test' in ${index}`);
+        }
+      }
+      // *************** validate logical operator if blockPassConditionsInput have more than one element
+      if (subjectPassConditionsInput.length > 1) {
+        if (!subjectPassConditionsInput[1].logical_operator) {
+          throw new ApolloError('logical operator is required for multiple pass condition');
+        }
+      }
+    });
+}
 // *************** EXPORT MODULE ***************
-module.exports = { ValidateSubjectInput, ValidateSubjectFilterInput };
+module.exports = { ValidateSubjectInput, ValidateSubjectFilterInput, ValidateSubjectPassConditionsInput };
