7bf8d6a948c0bd031b0886005f1199878c20b7f4
zettacamp-nabil-ilyasa <nabil.ilyasa@zettacamp.pro>
2025-07-18 11:33:59 +0700

    [Module_3][BE-019][ZEBN-28] Add CalculationResult Typedef

---
+++ modules/calculationResult/calculation_result.typedef.js
@@ -0,0 +1,83 @@
+// *************** IMPORT LIBRARY ***************
+const { gql } = require('apollo-server-express');
+
+const calculationResultTypeDefs = gql`
+  type CalculationResult {
+    student_id: Student
+    overall_result: GradingResultEnum
+    results: [BlockResult]
+    status: CalculationResultStatus
+    created_at: Date
+    created_by: User
+    updated_at: Date
+    deleted_at: Date
+    deleted_by: User
+  }
+
+  type BlockResult {
+    block_id: Block
+    block_result: GradingResultEnum
+    total_marks: Number
+    subject_results: [SubjectResult]
+  }
+
+  type SubjectResult {
+    subject_id: Subject
+    subject_result: GradingResultEnum
+    total_mark: Number
+    test_results: [TestResult]
+  }
+
+  type TestResult {
+    test_id: Test
+    test_result: GradingResultEnum
+    average_mark: Number
+    weighted_mark: Number
+  }
+
+  input CalculationResultInput {
+    student_id: Student
+    overall_result: GradingResultEnum
+    results: [BlockResultInput]
+  }
+
+  input BlockResultInput {
+    block_id: Block
+    block_result: GradingResultEnum
+    total_marks: Number
+    subject_results: [SubjectResultInput]
+  }
+
+  type SubjectResultInput {
+    subject_id: Subject
+    subject_result: GradingResultEnum
+    total_mark: Number
+    test_results: [TestResultInput]
+  }
+
+  type TestResultInput {
+    test_id: Test
+    test_result: GradingResultEnum
+    average_mark: Number
+    weighted_mark: Number
+  }
+
+  enum GradingResultEnum {
+    pass
+    fail
+  }
+
+  enum CalculationResultStatus {
+    active
+    deleted
+    archived
+  }
+
+  type extend query{
+    GetAllCalculationResults(): [CalculationResult]
+    GetAllCalculationResults(_id: ID!): CalculationResult
+  }
+`;
+
+// *************** EXPORT MODULE ***************
+module.exports = calculationResultTypeDefs;
