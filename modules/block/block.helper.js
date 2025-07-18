dac96bb34e1e640e078d89d5c87a7ef5ebe793a6
zettacamp-nabil-ilyasa <nabil.ilyasa@zettacamp.pro>
2025-07-18 14:30:32 +0700

    [Module_3][BE-006][ZEBN-27] Add BlockPassConditionPayloadComposer for helper

--- modules/block/block.helper.js
+++ modules/block/block.helper.js
@@ -17,5 +17,40 @@ function BlockPayloadComposer({ blockName, blockDescription }) {
   return blockPayload;
 }
 
+function BlockPassConditionsPayloadComposer(passConditionsInput) {
+  let passConditionPayload = [];
+  passConditionsInput.forEach((passCondition, index) => {
+    // *************** sanity check for parameter
+    if (!passCondition.parameter) throw new ApolloError(`pass condition's parameter in ${index} is required`);
+
+    // *************** sanity check for parameter_value
+    if (!passCondition.parameter_value) throw new ApolloError(`pass condition's parameter_value in ${index}  is required`);
+
+    // *************** sanity check for syllabus_type
+    if (!passCondition.syllabus_type) throw new ApolloError(`pass condition's syllabus_type in ${index}  is required`);
+
+    // *************** sanity check for math_operator
+    if (!passCondition.math_operator) throw new ApolloError(`pass condition's math_operator in ${index}  is required`);
+
+    let payloadObject = {
+      parameter: passCondition.parameter,
+      parameter_value: passCondition.parameter_value,
+      syllabus_type: passCondition.syllabus_type,
+      math_operator: passCondition.math_operator,
+    };
+
+    // *************** make sure to include subject_id if syllabus type is 'subject'
+    if (passCondition.syllabus_type === 'subject') {
+      if (!passCondition.subject_id) throw new ApolloError(`pass condition's subject_id in ${index} is required`);
+      payloadObject.subject_id = passCondition.subject_id;
+
+      // *************** make sure to include test_id if syllabus type is 'test'
+    } else if (passCondition.syllabus_type === 'test') {
+      if (!passCondition.test_id) throw new ApolloError(`pass condition's test_id in ${index} is required`);
+      payloadObject.test_id = passCondition.test_id;
+    }
+  });
+}
+
 // *************** EXPORT MODULE ***************
-module.exports = { BlockPayloadComposer };
+module.exports = { BlockPayloadComposer, BlockPassConditionsPayloadComposer };
