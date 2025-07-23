159bd80b68273b23e520c34e07c2580423c20763
zettacamp-nabil-ilyasa <nabil.ilyasa@zettacamp.pro>
2025-07-24 00:08:51 +0700

    [Module_3][BE-011][ZEBN-27] Update SubjectPassConditionPayloadComposer

--- modules/subject/subject.helper.js
+++ modules/subject/subject.helper.js
@@ -80,10 +80,6 @@ function SubjectPassConditionsPayloadComposer(passConditionsInput) {
     // *************** insert payload into array
     passConditionsPayload.push(payloadObject);
   });
-
-  // *************** verify result's length
-  if (passConditionsPayload.length !== passConditionsInput.length) throw new ApolloError('missmatch between payload result and input');
-  return passConditionsPayload;
 }
 
 // *************** EXPORT MODULE ***************
