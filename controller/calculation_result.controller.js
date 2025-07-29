3e6a2141fec896bd1b49b23bf6dde0ce7d4afc67
zettacamp-nabil-ilyasa <nabil.ilyasa@zettacamp.pro>
2025-07-29 09:59:29 +0700

    [MODULE_4][ZEBN-30][BE_002]Create PDF Generation Endpoint: Update calculation_result.controller.js

--- controller/calculation_result.controller.js
+++ controller/calculation_result.controller.js
@@ -5,28 +5,52 @@ const Mongoose = require('mongoose');
 const CalculationResultModel = require('../modules/calculationResult/calculation_result.model');
 const { GenerateTranscriptPdf } = require('../modules/calculationResult/calculation_result.helper');
 
-async function GenerateTranscriptPdfController(req, res) {
+/**
+ * Generate a transcript PDF file for a specific student and return it as a downloadable response.
+ * @param request - Express request object containing the student ID in URL parameters.
+ * @param response - Express response object used to send back the PDF buffer.
+ * @returns {Promise<void>} Sends a PDF buffer as HTTP response.
+ * @throws {500} Internal Server Error - If an unexpected error occurs during the process.
+ */
+async function GenerateTranscriptPdfController(request, response) {
   try {
-    const studentId = req?.params?.studentId;
+    // *************** get student's id from url parameter
+    const studentId = request?.params?.studentId;
+
+    // *************** check if studentId  is a valid mongoose objectid
     if (!Mongoose.Types.ObjectId.isValid(studentId)) {
-      return res.status(400).json({
+      return response.status(400).json({
         status: 400,
         message: 'studentId is required as parameter',
       });
     }
 
-    const calculationResultDocument = await CalculationResultModel.findOne({ student_id: studentId, status: 'active' });
-    if (!calculationResultDocument) {
-      return res.status(400).json({
+    // *************** get calculation result document via studentId, populate necessarry data
+    const populatedCalculationResultDocument = await CalculationResultModel.findOne({ student_id: studentId, status: 'active' })
+      .populate({ path: 'student_id', populate: { path: 'school_id' } })
+      .populate({ path: 'results.block_id' })
+      .populate({ path: 'results.subject_results.subject_id' })
+      .populate({ path: 'results.subject_results.test_results.test_id' })
+      .lean();
+
+    // *************** sanity check for the populated document
+    if (!populatedCalculationResultDocument) {
+      return response.status(400).json({
         status: 404,
         message: 'calculation result document not found',
       });
     }
 
-    const outputPdfFile = await GenerateTranscriptPdf;
-    res.send(outputPdfFile);
+    // *************** call GenerateTranscriptPdf helper to generate the pdf
+    const outputPdfFile = await GenerateTranscriptPdf(populatedCalculationResultDocument);
+    // *************** set response header
+    response.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': `attachment; filename=${populatedCalculationResultDocument.student_id._id}_${populatedCalculationResultDocument.student_id.last_name}_Transcript_Results.pdf`,
+    });
+    response.send(outputPdfFile);
   } catch (error) {
-    return res.status(500).json({
+    return response.status(500).json({
       status: 500,
       error: error.message,
     });
--- index.js
+++ index.js
@@ -19,11 +19,6 @@ async function InitializeServer() {
     // *************** initialize express app
     const app = InitializeExpressApp();
     app.use(router);
-    app._router.stack.forEach((r) => {
-      if (r.route && r.route.path) {
-        console.log(`Route registered: [${Object.keys(r.route.methods).join(',').toUpperCase()}] ${r.route.path}`);
-      }
-    });
 
     // *************** initialize apollo server
     const server = InitializeApolloServer();
